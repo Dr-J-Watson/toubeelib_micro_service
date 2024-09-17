083b1a5c92dada399202f84fa76acc5390426eb7
Dr-J-Watson <ptibandi54@gmail.com>
2024-09-17 14:32:14 +0200

    Corect use ArrayRdvRepo

--- app/src/infrastructure/repositories/ArrayRdvRepository.php
+++ app/src/infrastructure/repositories/ArrayRdvRepository.php
@@ -4,21 +4,20 @@ namespace toubeelib\infrastructure\repositories;
 
 use Ramsey\Uuid\Uuid;
 use toubeelib\core\domain\entities\rdv\RDV;
-use toubeelib\core\domain\entities\rdv\RendezVous;
 use toubeelib\core\dto\RDVDTO;
-use toubeelib\core\repositoryInterfaces\RdvRepositoryInterface;
+use toubeelib\core\repositoryInterfaces\RDVRepositoryInterface;
 use toubeelib\core\repositoryInterfaces\RepositoryEntityNotFoundException;
 
-class ArrayRdvRepository implements RdvRepositoryInterface
+class ArrayRdvRepository implements RDVRepositoryInterface
 {
     private array $rdvs = [];
 
     public function __construct() {
-            $r1 = new RendezVous('p1', 'pa1', 'A', \DateTimeImmutable::createFromFormat('Y-m-d H:i','2024-09-02 09:00') );
+            $r1 = new RDV('p1', 'pa1', 'A', \DateTimeImmutable::createFromFormat('Y-m-d H:i','2024-09-02 09:00') );
             $r1->setID('r1');
-            $r2 = new RendezVous('p1', 'pa1', 'A', \DateTimeImmutable::createFromFormat('Y-m-d H:i','2024-09-02 10:00'));
+            $r2 = new RDV('p1', 'pa1', 'A', \DateTimeImmutable::createFromFormat('Y-m-d H:i','2024-09-02 10:00'));
             $r2->setID('r2');
-            $r3 = new RendezVous('p2', 'pa1', 'A', \DateTimeImmutable::createFromFormat('Y-m-d H:i','2024-09-02 09:30'));
+            $r3 = new RDV('p2', 'pa1', 'A', \DateTimeImmutable::createFromFormat('Y-m-d H:i','2024-09-02 09:30'));
             $r3->setID('r3');
 
         $this->rdvs  = ['r1'=> $r1, 'r2'=>$r2, 'r3'=> $r3 ];
