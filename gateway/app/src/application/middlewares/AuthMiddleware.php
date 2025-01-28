6ac58bb29a77799eafda424c800ec91fe9ce5729
Dr-J-Watson <ptibandi54@gmail.com>
2025-01-28 17:52:37 +0100

    💩 Problème de midlewar

--- gateway/app/src/application/middlewares/AuthMiddleware.php
+++ gateway/app/src/application/middlewares/AuthMiddleware.php
@@ -2,24 +2,48 @@
 
 namespace gateway\application\middlewares;
 
+use Slim\Exception\HttpUnauthorizedException;
+use Slim\Exception\HttpInternalServerErrorException;
 use Psr\Http\Message\ServerRequestInterface as Request;
-use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
-use Slim\Psr7\Factory\ResponseFactory;
+use Psr\Http\Message\ResponseInterface as Response;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ConnectException;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\ServerException;
 
-$token_line = $request->hasHeader('Authorization');
-list($token) = sscanf($token_line, "Bearer %s");
+class AuthMiddleware
+{
+    private $auth_client;
 
-try {
-    $response = $this->auth_service->request('POST', '/tokens/validate', [
-        'json' => ['token' => $token]
-    ]);
-} catch (ConnectException | ServerException $e) {
-    throw new HttpInternalServerErrorException($request, "internal server error ({$e->getCode()}, {$e->getMessage()})");
-} catch (ClientException $e) {
-    match($e->getCode()) {
-        401 => throw new HttpUnauthorizedException($request, "unauthorized ({$e->getCode()}, {$e->getMessage()})"),
-    };
+    public function __construct(Client $auth_client)
+    {
+        $this->auth_client = $auth_client;
+    }
+
+    public function __invoke(Request $request, RequestHandler $handler): Response
+    {
+        $token_line = $request->getHeaderLine('Authorization');
+        list($token) = sscanf($token_line, "Bearer %s");
+
+        if (!$token) {
+            throw new HttpUnauthorizedException($request, "Token not provided");
+        }
+
+        try {
+        
+            $response = $this->auth_client->request('POST', '/tokens/validate', [
+                'json' => ['token' => $token]
+            ]);
+            
+        } catch (ConnectException | ServerException $e) {
+            throw new HttpInternalServerErrorException($request, "Internal server error ({$e->getCode()}, {$e->getMessage()})");
+        } catch (ClientException $e) {
+            if ($e->getCode() === 401) {
+                throw new HttpUnauthorizedException($request, "Unauthorized ({$e->getCode()}, {$e->getMessage()})");
+            }
+        }
+
+        return $handler->handle($request);
+    }
 }
-
-return $handler->handle($request);
