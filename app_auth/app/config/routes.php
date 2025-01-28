77c240f307d337b4bde29c275288bd3dd623fc5b
Antoine <a.rubeo-lisa@workadventu.re>
2025-01-28 14:28:55 +0100

    add auth route in app_auth

--- app_auth/app/config/routes.php
+++ app_auth/app/config/routes.php
@@ -1,28 +1,20 @@
 <?php
 declare(strict_types=1);
 
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Psr\Http\Message\ResponseInterface as Response;
-use app_auth\application\actions\AuthAction;
-use app_auth\application\actions\CancelRDVAction;
-use app_auth\application\actions\CreatePraticienAction;
-use app_auth\application\actions\CreateRDVAction;
-use app_auth\application\actions\GetPatientRDVAction;
-use app_auth\application\actions\GetPraticienPlanningAction;
-use app_auth\application\actions\GetRDVAction;
-use app_auth\application\actions\HomeAction;
-use app_auth\application\actions\UpdateRDVCycleAction;
-use app_auth\application\middlewares\JWTAuthMiddleware;
-use app_auth\application\middlewares\Cors;
+use app_auth\application\actions\RefreshTokenAuthAction;
+use app_auth\application\actions\SigninAuthAction;
+use app_auth\application\actions\ValidateTokenAuthAction;
+
+//use app_auth\application\actions\HomeAction;
 
 return function( \Slim\App $app): \Slim\App {
 
     // Authentification
-    $app->post('/users/signin[/]', AuthAction::class)->setName('signin');
+    $app->post('/users/signin[/]', SigninAuthAction::class)->setName('signin');
 
     // Rafraîchir le token JWT
-    $app->post('/users/refresh', AuthAction::class)->setName('refresh');
-        // ->add(new JWTAuthMiddleware());
+    $app->post('/users/refresh', RefreshTokenAuthAction::class)->setName('refresh');
 
+    $app->post('/users/validate', ValidateTokenAuthAction::class)->setName('refresh');
     return $app;
 };
---
+++ app_auth/app/src/application/actions/ValidateTokenAuthAction.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app_auth\application\actions;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use app_auth\application\providers\auth\JWTAuthnProvider;
+use app_auth\core\dto\AuthDTO;
+use app_auth\core\dto\CredentialsDTO;
+
+class ValidateTokenAuthAction extends AbstractAction{
+
+    public function __invoke(Request $rq, Response $rs, $args): Response{
+
+
+
+
+        return $rs;
+    }
+}
